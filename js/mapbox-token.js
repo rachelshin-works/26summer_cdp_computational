405f57e01422257a57b0d0eadd34852c6e1ca741
rachelshin-works <hs5584@nyu.edu>
2026-07-22 23:47:09 -0400

    Update mapbox-token.js
    add chinatown.html using mapbox

--- js/mapbox-token.js
+++ js/mapbox-token.js
@@ -1,3 +1,1 @@
-/* Paste your Mapbox public token below (pk....)
-   Create one at: https://account.mapbox.com/access-tokens/ */
 window.MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoicmFjaGVsc2hpbjA0MTUiLCJhIjoiY21xanhvdWcwMGwydjJxcHpoeTRpaXVrYyJ9.FZ3AassawGcSOfJu266JNg";
